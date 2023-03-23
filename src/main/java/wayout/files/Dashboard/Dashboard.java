edb7857551935d7b79d478068ad1b4e3ec91209f
staywithabdullah <masudbuiyan50@gmail.com>
2023-03-23 10:36:38 +0600

    demo dashboard

--- src/main/java/wayout/files/Dashboard/Dashboard.java
+++ src/main/java/wayout/files/Dashboard/Dashboard.java
@@ -1,4 +1,69 @@
 package wayout.files.Dashboard;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Dashboard {
+
+    @FXML
+    private MFXButton aboutus;
+
+    @FXML
+    private MFXButton cart;
+
+    @FXML
+    private MFXButton emergency;
+
+    @FXML
+    private MFXButton find_guide;
+
+    @FXML
+    private MFXButton find_tour;
+
+    @FXML
+    private MFXButton login_btn;
+
+    @FXML
+    private MFXScrollPane scrollbar;
+
+    @FXML
+    private MFXButton settings;
+
+    @FXML
+    void aboutus_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cart_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void emergency_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void find_guide_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void find_tour_clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void login_btn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settings_clicked(ActionEvent event) {
+
+    }
+
 }
