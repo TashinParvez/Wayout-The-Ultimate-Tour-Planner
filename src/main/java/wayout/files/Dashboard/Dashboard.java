61608bb6b03a16b6c75b7c9a1695240bafe83703
Tashin Parvez <tashinparvez2002@gmail.com>
2023-03-26 13:55:34 +0600

    Dashboad class + controller + fxml added just

--- src/main/java/wayout/files/Dashboard/Dashboard.java
+++ src/main/java/wayout/files/Dashboard/Dashboard.java
@@ -1,4 +1,23 @@
 package wayout.files.Dashboard;
 
-public class Dashboard {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Dashboard extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
